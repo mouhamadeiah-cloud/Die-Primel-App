a65fd58d6cd3d642af38ad9c60ea4429addde81c
mouhamadeiah-cloud <mouhamadeiah@googlemail.com>
2026-02-16 18:00:00 +0100

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,47 +1,67 @@
-// This is the "Offline page" service worker
+const CACHE_NAME = 'primel-pos-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.1/firebase-database-compat.js',
+  'https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3',
+  'https://assets.mixkit.co/active_storage/sfx/2870/2870-preview.mp3'
+];
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
+// Installation
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache geöffnet');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
+// Fetch-Intercept
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
         }
+        return fetch(event.request).then(
+          response => {
+            // Prüfen ob gültige Response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
+            // Response klonen
+            const responseToCache = response.clone();
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+// Alte Caches löschen
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
